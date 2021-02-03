0b2b6b7e195d4165f4105c7c4cf84cde8af76020
NecmettinCalibasi <48380744+NecmettinCalibasi@users.noreply.github.com>
2021-02-03 09:29:44 +0100

    Add files via upload

---
+++ BMI-Calculator-iOS13/BMI.swift
@@ -0,0 +1,15 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Necmettin Çalıbaşı on 8/23/20.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
