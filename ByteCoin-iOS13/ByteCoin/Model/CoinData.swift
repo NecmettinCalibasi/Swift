01c3007484eb99d1528a104ac88a468695fadb2f
NecmettinCalibasi <48380744+NecmettinCalibasi@users.noreply.github.com>
2021-02-11 09:27:07 +0100

    Add files via upload

---
+++ ByteCoin-iOS13/ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Necmettin Çalıbaşı on 3/12/20.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Codable {
+    let rate: Double
+}
